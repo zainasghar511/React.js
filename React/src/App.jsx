a7909b115775bf7999dee705c809655187428d6d
zainasghar511 <zainasghae6@gmail.com>
2026-07-11 23:29:45 +0500

    add css tailwind and inputs

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,33 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Add from './Add'
 
 const App = () => {
-  const [first, setFirst] = useState("")
-  const [Addlist, setAddlist] = useState([]);
-
-   const handlepush = () =>{
-    if(! first.trim()) return
-    const newitem = {
-      text: first,
-      addedAt: new Date().toLocaleTimeString(),
-      
-
-    };
-    setAddlist(prevList=>[...prevList,newitem])
-    setFirst("")
-   }
   return (
     <div>
-<input type="text" placeholder='write here' value={first} onChange={(e)=>setFirst(e.target.value)} />
-<button onClick={handlepush} >Done</button>
-<ul>
-  {Addlist.map((item,index)=>{
-    return(
-<li key={index}>
-  <p>{item.text}</p>
-<span>{item.addedAt}</span>
-</li>)
-  })}
-</ul>
+<Add/>
     </div>
   )
 }
